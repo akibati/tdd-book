38971bc0d4e5db0987ba5a4ea9ecee7caef1cdd9
a.chiba <a.chiba@uchida-it.co,jp>
2024-10-17 22:29:10 +0900

    p46

--- TDDTest.Tests/MoneyTest.cs
+++ TDDTest.Tests/MoneyTest.cs
@@ -25,6 +25,7 @@ namespace TDDTest.Tests
             Assert.That(new Dollar(5).Equals(new Dollar(6)), Is.False);
             Assert.That(new Franc(5).Equals(new Franc(5)), Is.True);
             Assert.That(new Franc(5).Equals(new Franc(6)), Is.False);
+            Assert.That(new Franc(5).Equals(new Dollar(5)), Is.False);
         }
 
         [Test]
